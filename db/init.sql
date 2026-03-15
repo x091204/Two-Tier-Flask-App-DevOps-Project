197f729911535530e909594a353a95f02b068601
x091204 <akifmuhammed009@gmail.com>
2026-03-15 18:48:35 +0530

    updated sql file

--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,9 @@
-from flask import Flask
-from flask_mysqldb import MySQL
-import os
+CREATE DATABASE IF NOT EXISTS student_db;
+USE student_db;
 
-app = Flask(__name__)
-
-app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST", "mysql")
-app.config["MYSQL_USER"] = os.getenv("MYSQL_USER", "root")
-app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD", "DevOps@123")
-app.config["MYSQL_DB"] = os.getenv("MYSQL_DB", "student_db")
-
-mysql = MySQL(app)
+CREATE TABLE IF NOT EXISTS students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    roll_no VARCHAR(50) NOT NULL,
+    grade VARCHAR(10) NOT NULL
+);
