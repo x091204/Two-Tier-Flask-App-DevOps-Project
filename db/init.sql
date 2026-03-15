aa6a49c147744c849b6ee3e0629356119210e8df
x091204 <akifmuhammed009@gmail.com>
2026-03-15 18:10:28 +0530

    updated docker compsoe

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,12 @@
+from flask import Flask
+from flask_mysqldb import MySQL
 import os
 
-MYSQL_HOST = os.getenv("MYSQL_HOST", "mysql")
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "DevOps@123")
-MYSQL_DB = os.getenv("MYSQL_DB", "student_db")
+app = Flask(__name__)
+
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST", "mysql")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER", "root")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD", "DevOps@123")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB", "student_db")
+
+mysql = MySQL(app)
